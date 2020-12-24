7cbec503602606e6d5799d82f44902d35605bbd2
Branyes N.A <branyernunez@gmail.com>
2020-12-24 01:16:31 -0400

    Agregando funcionalidad Update

--- coches.js
+++ coches.js
@@ -5,7 +5,8 @@ const coches = [
         model:"Camry",
         color: "Red",
         year:   "2010",
-        image: "https://gossipvehiculo.com/wp-content/uploads/2019/09/2020-toyota-camry-trd-first-drive.jpg"
+        image: "https://gossipvehiculo.com/wp-content/uploads/2019/09/2020-toyota-camry-trd-first-drive.jpg",
+        price: "RD$2,0000"
     },
     {
         id:2,
@@ -13,7 +14,8 @@ const coches = [
         model:"Civic",
         color: "Grey",
         year:   "2011",
-        image: "https://i.pinimg.com/236x/66/4c/c9/664cc9adc4546328b01890c91140938f.jpg"
+        image: "https://i.pinimg.com/236x/66/4c/c9/664cc9adc4546328b01890c91140938f.jpg",
+        price: "RD$2,0000"
     },
     {
         id:3,
@@ -21,7 +23,8 @@ const coches = [
         model:"Sonata",
         color: "Orange",
         year:   "2012",
-        image: "https://upload.wikimedia.org/wikipedia/commons/5/58/2014_Hyundai_Sonata_%28YF%29_LPi_Taxi_by_seoul.JPG"
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/58/2014_Hyundai_Sonata_%28YF%29_LPi_Taxi_by_seoul.JPG",
+        price: "RD$2,0000"
     },
     {
         id:4,
@@ -29,7 +32,8 @@ const coches = [
         model:"LXZ",
         color: "Blue",
         year:   "2013",
-        image: "https://ddjkm7nmu27lx.cloudfront.net/149521125549255/cover/59254706f1e0d4.65562190.jpg"
+        image: "https://ddjkm7nmu27lx.cloudfront.net/149521125549255/cover/59254706f1e0d4.65562190.jpg",
+        price: "RD$2,0000"
     },
     {
         id:5,
@@ -37,7 +41,8 @@ const coches = [
         model:"Cayenne",
         color: "Black",
         year:   "2020",
-        image: "https://pict1.reezocar.com/images/660/gebrauchtwagen.at/RZCGBWAT022AF5C08952/PORSCHE-CAYENNE-00.jpg"
+        image: "https://pict1.reezocar.com/images/660/gebrauchtwagen.at/RZCGBWAT022AF5C08952/PORSCHE-CAYENNE-00.jpg",
+        price: "RD$2,0000"
     },
     {
         id:6,
@@ -45,7 +50,8 @@ const coches = [
         model:"Turbo",
         color: "White",
         year:   "2019",
-        image: "https://s1.cdn.autoevolution.com/images/news/ferrari-458-italia-to-go-turbo-in-2015-81998_1.jpg"
+        image: "https://s1.cdn.autoevolution.com/images/news/ferrari-458-italia-to-go-turbo-in-2015-81998_1.jpg",
+        price: "RD$2,0000"
     },
 ]
 
@@ -63,9 +69,10 @@ function printCars(){
                     <td>${coches.model}</td>
                     <td>${coches.year}</td>
                     <td>${coches.color}</td>
+                    <td>${coches.price}</td>
                     <td>
-                        <button onclick="deleteCars(${coches.id})" class="btn btn-primary"">
-                        Update
+                        <button onclick="updateCars(${coches.id})" class="btn btn-primary"">
+                       Update
                     </button>
                     <button onclick="deleteCars(${coches.id})" class="btn btn-danger">
                    Delete
@@ -91,7 +98,10 @@ function addCars() {
     const model = document.getElementById('model').value;
     const year = document.getElementById('year').value;
     const color = document.getElementById('color').value;
+    const price = document.getElementById('price').value;
     const image = document.getElementById('image').value;
+
+   
 
     const id = coches[coches.length -1].id + 1;
 
@@ -100,19 +110,49 @@ function addCars() {
         brand,
         model,
         color,
+        price,
         year,
         image,
     }
+    if(brand=="" && model=="" && year=="" && color== "" && price==""){
+        alert("Por favor revisa que allas llenado todos los campos de manera correcta...")
+    }else{
     coches.push(newCars);
+    }
     printCars();
 
-    // limpiamos el formulario
+
     document.getElementById('form-cars').reset();
 }
 
 printCars();
 
+function updateCars(id){
+    const brand = document.getElementById('brand').value  
+    const model = document.getElementById('model').value 
+    const year = document.getElementById('year').value 
+    const color = document.getElementById('color').value
+    const price = document.getElementById('price').value;
+    const image = document.getElementById('image').value 
 
+    const updateCars = {
+        id,
+        brand,
+        model,
+        year,
+        color,
+        price,
+        image,
+    }
+    if(brand=="" && model=="" && year=="" && color== "" && price== ""){
+        alert("Por favor revisa que allas llenado todos los campos de manera correcta...")
+    }else{
+    coches.splice(id-1,1,updateCars)
+    }
+    document.getElementById('form-cars').reset();
+    printCars();
+
+}
 
 
 
